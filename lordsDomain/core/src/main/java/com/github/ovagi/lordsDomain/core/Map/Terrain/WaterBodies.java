42d9bd18d01e8858460a9e174869ecb5200c24c0
Tim Clark <tim.clark@genesys.com>
2019-04-15 11:16:11 -0400

    Updated Water body algorithm

--- lordsDomain/core/src/main/java/com/github/ovagi/lordsDomain/core/Map/Terrain/WaterBodies.java
+++ lordsDomain/core/src/main/java/com/github/ovagi/lordsDomain/core/Map/Terrain/WaterBodies.java
@@ -6,15 +6,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static com.github.ovagi.lordsDomain.core.Map.Map.DRAWING_WATER_SHED;
+import static com.github.ovagi.lordsDomain.core.Map.Map.NUMBER_OF_CELLS;
 
 //https://medium.com/universe-factory/how-i-generated-artificial-rivers-on-imaginary-continents-747df12c5d4c
 
 public class WaterBodies {
 
-    private static final int NUMBER_OF_RIVERS = 5;
     public static final float OCEAN_HEIGHT = (float) ThreadLocalRandom.current().nextDouble(0, 1.0 / 3.0);
-    private static final int MAX_RIVER_LENGTH = 25;
-    private static final int MAX_TIME_STEP = 100;
+    private static final int MIN_RIVER_LENGTH = NUMBER_OF_CELLS / 8;
+    private static final int MAX_RIVER_LENGTH = NUMBER_OF_CELLS / 4;
     private static final double RIVER_HEIGHT = (float) ThreadLocalRandom.current().nextDouble(3.0 / 10.0, 4.0 / 10.0);
 
     public WaterBodies(ArrayList<Cell> cells) {
@@ -25,65 +25,61 @@ public class WaterBodies {
         System.out.println("LOG: Starting River Generation");
         cells.forEach(cell -> cell.setDrainsIntoMe(new HashSet<>()));
         cells.forEach(cell -> cell.setWaterFill(cell.getElevation()));
-        double maxDrains = 0;
-
-
         //Find Spawn Point of River
             //First find border cells
-        Cell randomBroder = getRandomCell(cells.parallelStream().filter(cell -> cell.getNeighboringCells().size() < 8).collect(Collectors.toCollection(ArrayList::new)));
-        randomBroder
-
+        Cell start = getRandomCell(cells.parallelStream().filter(cell -> cell.getNeighboringCells().size() < 4).collect(Collectors.toCollection(ArrayList::new)));
+        start.setTerrainType(TerrainTypes.RIVER);
 
         //Find End Point of River
+        Cell end = getRandomCell(cells.parallelStream().filter(cell -> cell.getNeighboringCells().size() < 4).collect(Collectors.toCollection(ArrayList::new)));
 
-        //
 
-        for (Cell cell : cells) {
-            boolean draining = false;
-            do {
-                Cell min = cell.getNeighboringCells().parallelStream().min(Comparator.comparingDouble(Cell::getElevation)).orElse(null);
-                if (min != null) {
-                    if (min.getElevation() < cell.getElevation()) {
-                        min.getDrainsIntoMe().add(cell);
-                        min.getDrainsIntoMe().addAll(cell.getDrainsIntoMe());
-                        if (min.getDrainsIntoMe().size() > maxDrains) {
-                            maxDrains = min.getDrainsIntoMe().size();
-                        }
-                        cell = min;
-                        draining = true;
+        //Check that end is not on the same column / row as start
+        boolean checking = true;
+        ArrayList<Cell> edgeCells = start.getNeighboringCells().parallelStream().filter(cell -> cell.getNeighboringCells().size() < 4).collect(Collectors.toCollection(ArrayList::new));
+        boolean foundEnd = false;
+        do {
+            while (checking && !foundEnd) {
+                Cell finalEnd = end;
+                foundEnd = edgeCells.parallelStream().anyMatch(cell -> cell.equals(finalEnd));
 
-                    } else {
-                        System.out.println("DEBUG: Found Non-Draining Cell");
-                        draining = false;
-                    }
-                }
-            } while (draining);
-        }
 
-        for (Cell cell: cells) {
-            cell.setWaterFill(cell.getDrainsIntoMe().size() / maxDrains);
-            if(cell.getWaterFill() > RIVER_HEIGHT) {
-                cell.setTerrainType(TerrainTypes.RIVER);
+                edgeCells = edgeCells.parallelStream()
+                        .filter(cell -> cell.getNeighboringCells().size() == 3)
+                        .map(Cell::getNeighboringCells)
+                        .flatMap(Collection::parallelStream)
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+                checking = edgeCells.isEmpty();
             }
-        }
+            end = getRandomCell(cells.parallelStream().filter(cell -> cell.getNeighboringCells().size() < 4).collect(Collectors.toCollection(ArrayList::new)));
+            checking = false;
+        }while (foundEnd);
+
+        end.setTerrainType(TerrainTypes.RIVER);
+
+        do {
+            start = start.getNeighboringCells().parallelStream()
+                    .filter(cell -> cell.getNeighboringCells().size() < 4 && !cell.getTerrainType().equals(TerrainTypes.RIVER))
+                    .min(Comparator.comparingDouble(Cell::getElevation))
+            .orElse(null);
+
+            if(start == null) {
+                throw new RuntimeException("Start can not be null");
+            }
+
+            start.setTerrainType(TerrainTypes.RIVER);
+        } while(!start.equals(end));
 
         System.out.println("LOG: Ending River Generation");
-//        for every cell
-//        find neighboring cells and store fill values
-//        set neighbor values to NULL where drain = 0
-//        get max neighbor value
-//        if max is higher than self fill
-//        set fill to max
-//        set drains to 1
-//        output fill
         //output fill
 
-        //        //Oceans
-        cells.parallelStream().forEach(cell -> {
-            if (cell.getElevation() < OCEAN_HEIGHT) {
-                cell.setTerrainType(TerrainTypes.OCEAN);
-            }
-        });
+        //Oceans
+//        cells.parallelStream().forEach(cell -> {
+//            if (cell.getElevation() < OCEAN_HEIGHT) {
+//                cell.setTerrainType(TerrainTypes.OCEAN);
+//            }
+//        });
 
     }
 
